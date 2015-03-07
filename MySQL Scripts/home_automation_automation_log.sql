c1b4c3484cb7be4677a2517386c16dbbf29b0d6a
Shawn Hoffman <shawn.p.hoffman@gmail.com>
2015-03-06 19:15:20 -0500

    Modified original DDL

--- MySQL Scripts/home_automation_automation_log.sql
+++ MySQL Scripts/home_automation_automation_log.sql
@@ -1,16 +1,16 @@
-CREATE DATABASE  IF NOT EXISTS `home_automation`;
-USE `home_automation`;
+CREATE DATABASE  IF NOT EXISTS WeatherStationDB;
+USE WeatherStationDB;
 
-DROP TABLE IF EXISTS `automation_log`;
-CREATE TABLE `automation_log` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `event_service` varchar(45) NOT NULL,
-  `event_name` varchar(45) NOT NULL,
-  `event_caller` varchar(45) DEFAULT NULL,
-  `event_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `notes` varchar(45) DEFAULT NULL,
-  `notes2` varchar(45) DEFAULT NULL,
-  `notes3` varchar(80) DEFAULT NULL,
-  `return_value` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+DROP TABLE IF EXISTS automation_log;
+CREATE TABLE automation_log (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  event_service varchar(45) NOT NULL,
+  event_name varchar(45) NOT NULL,
+  event_caller varchar(45) DEFAULT NULL,
+  event_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  notes varchar(45) DEFAULT NULL,
+  notes2 varchar(45) DEFAULT NULL,
+  notes3 varchar(80) DEFAULT NULL,
+  return_value int(11) DEFAULT NULL,
+  PRIMARY KEY (id)
 );
